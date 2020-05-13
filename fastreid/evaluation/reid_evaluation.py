320010f2ae460e77a8072e77156c359da5c8f71d
liaoxingyu <sherlockliao01@gmail.com>
2020-05-13 11:47:52 +0800

    feat: support re-rank in test phase

--- fastreid/evaluation/reid_evaluation.py
+++ fastreid/evaluation/reid_evaluation.py
@@ -3,6 +3,7 @@
 @author:  liaoxingyu
 @contact: sherlockliao01@gmail.com
 """
+import logging
 import copy
 from collections import OrderedDict
 
@@ -12,10 +13,14 @@ import torch.nn.functional as F
 
 from .evaluator import DatasetEvaluator
 from .rank import evaluate_rank
+from .rerank import re_ranking
+
+logger = logging.getLogger(__name__)
 
 
 class ReidEvaluator(DatasetEvaluator):
     def __init__(self, cfg, num_query, output_dir=None):
+        self.cfg = cfg
         self._num_query = num_query
         self._output_dir = output_dir
 
@@ -29,14 +34,19 @@ class ReidEvaluator(DatasetEvaluator):
         self.camids = []
 
     def process(self, outputs):
-        self.features.append(outputs[0].cpu())
+        self.features.append(outputs[0])
         self.pids.extend(outputs[1].cpu().numpy())
         self.camids.extend(outputs[2].cpu().numpy())
 
+    @staticmethod
+    def cal_dist(query_feat: torch.tensor, gallery_feat: torch.tensor):
+        query_feat = F.normalize(query_feat, dim=1)
+        gallery_feat = F.normalize(gallery_feat, dim=1)
+        cos_dist = 1 - torch.mm(query_feat, gallery_feat.t()).cpu().numpy()
+        return cos_dist
+
     def evaluate(self):
         features = torch.cat(self.features, dim=0)
-        # normalize feature
-        features = F.normalize(features, dim=1)
 
         # query feature, person ids and camera ids
         query_features = features[:self._num_query]
@@ -50,8 +60,21 @@ class ReidEvaluator(DatasetEvaluator):
 
         self._results = OrderedDict()
 
-        cos_dist = torch.mm(query_features, gallery_features.t()).numpy()
-        cmc, all_AP, all_INP = evaluate_rank(1 - cos_dist, query_pids, gallery_pids, query_camids, gallery_camids)
+        dist = self.cal_dist(query_features, gallery_features)
+
+        if self.cfg.TEST.RERANK.ENABLED:
+            logger.info("Test with rerank setting")
+            k1 = self.cfg.TEST.RERANK.K1
+            k2 = self.cfg.TEST.RERANK.K1
+            lambda_value = self.cfg.TEST.RERANK.LAMBDA
+            q_q_dist = self.cal_dist(query_features, query_features)
+            g_g_dist = self.cal_dist(gallery_features, gallery_features)
+            dist = re_ranking(dist, q_q_dist, g_g_dist, k1, k2, lambda_value)
+
+        if self.cfg.TEST.AQE.ENABLED:
+            pass
+
+        cmc, all_AP, all_INP = evaluate_rank(dist, query_pids, gallery_pids, query_camids, gallery_camids)
         mAP = np.mean(all_AP)
         mINP = np.mean(all_INP)
         for r in [1, 5, 10]:
