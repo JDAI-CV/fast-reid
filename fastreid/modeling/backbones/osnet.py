3732f9440554b1aa5bac614d41c45d4b7cedc001
liaoxingyu <sherlockliao01@gmail.com>
2020-06-09 14:38:49 +0800

    update osnet

--- fastreid/modeling/backbones/osnet.py
+++ fastreid/modeling/backbones/osnet.py
@@ -175,10 +175,11 @@ class ChannelGate(nn.Module):
             layer_norm=False
     ):
         super(ChannelGate, self).__init__()
-        if num_gates is None:
-            num_gates = in_channels
+        if num_gates is None: num_gates = in_channels
         self.return_gates = return_gates
+
         self.global_avgpool = nn.AdaptiveAvgPool2d(1)
+
         self.fc1 = nn.Conv2d(
             in_channels,
             in_channels // reduction,
@@ -187,8 +188,7 @@ class ChannelGate(nn.Module):
             padding=0
         )
         self.norm1 = None
-        if layer_norm:
-            self.norm1 = nn.LayerNorm((in_channels // reduction, 1, 1))
+        if layer_norm: self.norm1 = nn.LayerNorm((in_channels // reduction, 1, 1))
         self.relu = nn.ReLU(inplace=True)
         self.fc2 = nn.Conv2d(
             in_channels // reduction,
@@ -197,12 +197,9 @@ class ChannelGate(nn.Module):
             bias=True,
             padding=0
         )
-        if gate_activation == 'sigmoid':
-            self.gate_activation = nn.Sigmoid()
-        elif gate_activation == 'relu':
-            self.gate_activation = nn.ReLU(inplace=True)
-        elif gate_activation == 'linear':
-            self.gate_activation = None
+        if gate_activation == 'sigmoid':  self.gate_activation = nn.Sigmoid()
+        elif gate_activation == 'relu':   self.gate_activation = nn.ReLU(inplace=True)
+        elif gate_activation == 'linear': self.gate_activation = nn.Identity()
         else:
             raise RuntimeError(
                 "Unknown gate activation: {}".format(gate_activation)
@@ -212,14 +209,11 @@ class ChannelGate(nn.Module):
         input = x
         x = self.global_avgpool(x)
         x = self.fc1(x)
-        if self.norm1 is not None:
-            x = self.norm1(x)
+        if self.norm1 is not None: x = self.norm1(x)
         x = self.relu(x)
         x = self.fc2(x)
-        if self.gate_activation is not None:
-            x = self.gate_activation(x)
-        if self.return_gates:
-            return x
+        x = self.gate_activation(x)
+        if self.return_gates: return x
         return input * x
 
 
@@ -259,8 +253,8 @@ class OSBlock(nn.Module):
         if in_channels != out_channels:
             self.downsample = Conv1x1Linear(in_channels, out_channels)
         self.IN = None
-        if IN:
-            self.IN = nn.InstanceNorm2d(out_channels, affine=True)
+        if IN: self.IN = nn.InstanceNorm2d(out_channels, affine=True)
+        self.relu = nn.ReLU(True)
 
     def forward(self, x):
         identity = x
@@ -276,7 +270,7 @@ class OSBlock(nn.Module):
         out = x3 + identity
         if self.IN is not None:
             out = self.IN(out)
-        return F.relu(out)
+        return self.relu(out)
 
 
 ##########
@@ -352,7 +346,7 @@ class OSNet(nn.Module):
             layers.append(
                 nn.Sequential(
                     Conv1x1(out_channels, out_channels),
-                    nn.AvgPool2d(2, stride=2)
+                    nn.AvgPool2d(2, stride=2),
                 )
             )
 
