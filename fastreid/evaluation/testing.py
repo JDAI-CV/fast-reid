cf06c9e8c7f938b1863dfab6748b9fcb5a54bdc7
liaoxingyu <sherlockliao01@gmail.com>
2020-07-15 15:53:03 +0800

    update tabulate vis with dataset name

--- fastreid/evaluation/testing.py
+++ fastreid/evaluation/testing.py
@@ -17,7 +17,7 @@ def print_csv_format(results):
     """
     assert isinstance(results, OrderedDict), results  # unordered results cannot be properly printed
     task = list(results.keys())[0]
-    metrics = [k for k in results[task]]
+    metrics = ["Datasets"] + [k for k in results[task]]
     logger = logging.getLogger(__name__)
 
     csv_results = []
