fae128a4dbb6869ea45cdf5be889c8b197364f8f
liaoxingyu <sherlockliao01@gmail.com>
2020-09-25 15:05:56 +0800

    fix bug in evaluator (#284)
    Summary: Change `Trainer` to `DefaultTrainer` in `tools/train.py` for adapting to new version of `build_evaluator`.

--- tools/train_net.py
+++ tools/train_net.py
@@ -5,8 +5,6 @@
 @contact: sherlockliao01@gmail.com
 """
 
-import logging
-import os
 import sys
 
 sys.path.append('.')
@@ -14,15 +12,6 @@ sys.path.append('.')
 from fastreid.config import get_cfg
 from fastreid.engine import DefaultTrainer, default_argument_parser, default_setup, launch
 from fastreid.utils.checkpoint import Checkpointer
-from fastreid.evaluation import ReidEvaluator
-
-
-class Trainer(DefaultTrainer):
-    @classmethod
-    def build_evaluator(cls, cfg, num_query, output_folder=None):
-        if output_folder is None:
-            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
-        return ReidEvaluator(cfg, num_query)
 
 
 def setup(args):
@@ -43,14 +32,14 @@ def main(args):
     if args.eval_only:
         cfg.defrost()
         cfg.MODEL.BACKBONE.PRETRAIN = False
-        model = Trainer.build_model(cfg)
+        model = DefaultTrainer.build_model(cfg)
 
         Checkpointer(model).load(cfg.MODEL.WEIGHTS)  # load trained model
 
-        res = Trainer.test(cfg, model)
+        res = DefaultTrainer.test(cfg, model)
         return res
 
-    trainer = Trainer(cfg)
+    trainer = DefaultTrainer(cfg)
     trainer.resume_or_load(resume=args.resume)
     return trainer.train()
 
