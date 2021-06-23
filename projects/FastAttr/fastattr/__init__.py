2fee8327fafd7df4f144e1f270e38718efe78e87
liaoxingyu <sherlockliao01@gmail.com>
2021-06-23 17:49:35 +0800

    minor fix for configs
    #519

--- projects/FastAttr/fastattr/__init__.py
+++ projects/FastAttr/fastattr/__init__.py
@@ -6,7 +6,6 @@
 
 from .attr_evaluation import AttrEvaluator
 from .config import add_attr_config
-from .data_build import build_attr_train_loader, build_attr_test_loader
 from .datasets import *
 from .modeling import *
 from .attr_dataset import AttrDataset
