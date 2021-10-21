d73fc53f8ac582996c559c3a5c66ab7050957ff6
l1aoxingyu <sherlockliao01@gmail.com>
2021-10-21 14:41:38 +0800

    feat: let compile evaluation cython automatically

--- fastreid/evaluation/reid_evaluation.py
+++ fastreid/evaluation/reid_evaluation.py
@@ -5,6 +5,7 @@
 """
 import copy
 import logging
+import time
 import itertools
 from collections import OrderedDict
 
@@ -17,8 +18,7 @@ from fastreid.utils import comm
 from fastreid.utils.compute_dist import build_dist
 from .evaluator import DatasetEvaluator
 from .query_expansion import aqe
-from .rank import evaluate_rank
-from .roc import evaluate_roc
+from .rank_cylib import compile_helper
 
 logger = logging.getLogger(__name__)
 
@@ -32,6 +32,7 @@ class ReidEvaluator(DatasetEvaluator):
         self._cpu_device = torch.device('cpu')
 
         self._predictions = []
+        self._compile_dependencies()
 
     def reset(self):
         self._predictions = []
@@ -102,6 +103,7 @@ class ReidEvaluator(DatasetEvaluator):
             rerank_dist = build_dist(query_features, gallery_features, metric="jaccard", k1=k1, k2=k2)
             dist = rerank_dist * (1 - lambda_value) + dist * lambda_value
 
+        from .rank import evaluate_rank
         cmc, all_AP, all_INP = evaluate_rank(dist, query_pids, gallery_pids, query_camids, gallery_camids)
 
         mAP = np.mean(all_AP)
@@ -113,6 +115,7 @@ class ReidEvaluator(DatasetEvaluator):
         self._results["metric"] = (mAP + cmc[0]) / 2 * 100
 
         if self.cfg.TEST.ROC.ENABLED:
+            from .roc import evaluate_roc
             scores, labels = evaluate_roc(dist, query_pids, gallery_pids, query_camids, gallery_camids)
             fprs, tprs, thres = metrics.roc_curve(labels, scores)
 
@@ -121,3 +124,20 @@ class ReidEvaluator(DatasetEvaluator):
                 self._results["TPR@FPR={:.0e}".format(fpr)] = tprs[ind]
 
         return copy.deepcopy(self._results)
+
+    def _compile_dependencies(self):
+        # Since we only evaluate results in rank(0), so we just need to compile
+        # cython evaluation tool on rank(0)
+        if comm.is_main_process():
+            try:
+                from .rank_cylib.rank_cy import evaluate_cy
+            except ImportError:
+                start_time = time.time()
+                logger.info("> compiling reid evaluation cython tool")
+
+                compile_helper()
+
+                logger.info(
+                    ">>> done with reid evaluation cython tool. Compilation time: {:.3f} "
+                    "seconds".format(time.time() - start_time))
+        comm.synchronize()
