ae842114c74dd5e4d23644f556e7745baee35ec6
zuchen.wang <zuchen.wang@vipshop.com>
2021-10-21 10:50:38 +0800

    update dataset config in trainer.py

--- projects/FastShoe/fastshoe/trainer.py
+++ projects/FastShoe/fastshoe/trainer.py
@@ -5,6 +5,8 @@
 import logging
 import os
 
+import torch
+
 from fastreid.data.build import _root
 from fastreid.engine import DefaultTrainer
 from fastreid.data.datasets import DATASET_REGISTRY
@@ -12,18 +14,10 @@ from fastreid.utils import comm
 from fastreid.data.transforms import build_transforms
 from fastreid.data.build import build_reid_train_loader, build_reid_test_loader
 from fastreid.evaluation.pair_evaluator import PairEvaluator
-
 from projects.FastShoe.fastshoe.data import PairDataset
 
 
 class PairTrainer(DefaultTrainer):
-
-    shoe_img_dir = os.path.join(_root, 'shoe_crop_all_images')
-    shoe_anno_dir = os.path.join(_root, 'labels/0930')
-
-    excel_img_dir = os.path.join(_root, 'excel/shoe_crop_images')
-    excel_anno_path = os.path.join(_root, 'excel/excel_pair_crop.csv')
-    # excel_anno_path = os.path.join(_root, 'excel/temp_test.csv')
 
     @classmethod
     def build_train_loader(cls, cfg):
@@ -32,15 +26,15 @@ class PairTrainer(DefaultTrainer):
 
         pos_folder_list, neg_folder_list = list(), list()
         for d in cfg.DATASETS.NAMES:
-            data = DATASET_REGISTRY.get(d)(img_dir=cls.shoe_img_dir,
-                                           anno_path=os.path.join(cls.shoe_anno_dir, '0930_clean_train.json'))
+            data = DATASET_REGISTRY.get(d)(img_dir=os.path.join(_root, 'shoe_crop_all_images'),
+                                           anno_path=os.path.join(_root, 'labels/1019/1019_clean_train.json'))
             if comm.is_main_process():
                 data.show_train()
             pos_folder_list.extend(data.train)
             neg_folder_list.extend(data.query)
 
         transforms = build_transforms(cfg, is_train=True)
-        train_set = PairDataset(img_root=cls.shoe_img_dir,
+        train_set = PairDataset(img_root=os.path.join(_root, 'shoe_crop_all_images'),
                                 pos_folders=pos_folder_list, neg_folders=neg_folder_list, transform=transforms, mode='train')
         data_loader = build_reid_train_loader(cfg, train_set=train_set)
         return data_loader
@@ -49,20 +43,36 @@ class PairTrainer(DefaultTrainer):
     def build_test_loader(cls, cfg, dataset_name):
         transforms = build_transforms(cfg, is_train=False)
         if dataset_name == 'ShoeDataset':
+            shoe_img_dir = os.path.join(_root, 'shoe_crop_all_images')
             if cfg.eval_only:
+                # for testing
                 mode = 'test'
-                anno_path = os.path.join(cls.shoe_anno_dir, '0930_clean_test.json')
+                anno_path = os.path.join(_root, 'labels/1019/1019_clean_test.json')
             else:
+                # for validation in train phase
                 mode = 'val'
-                anno_path = os.path.join(cls.shoe_anno_dir, '0930_clean_val.json')
+                anno_path = os.path.join(_root, 'labels/1019/1019_clean_val.json')
 
-            data = DATASET_REGISTRY.get(dataset_name)(img_dir=cls.shoe_img_dir, anno_path=anno_path)
-            test_set = PairDataset(img_root=cls.shoe_img_dir,
+            data = DATASET_REGISTRY.get(dataset_name)(img_dir=shoe_img_dir, anno_path=anno_path)
+            test_set = PairDataset(img_root=shoe_img_dir,
                                    pos_folders=data.train, neg_folders=data.query, transform=transforms, mode=mode)
         elif dataset_name == 'OnlineDataset':
-            test_set = DATASET_REGISTRY.get(dataset_name)(img_dir=cls.excel_img_dir,
-                                                          anno_path=cls.excel_anno_path,
-                                                          transform=transforms)
+            if cfg.eval_only:
+                # for testing
+                test_set_0830 = DATASET_REGISTRY.get(dataset_name)(img_dir=os.path.join(_root, 'excel/0830/shoe_crop_images'),
+                                                                   anno_path=os.path.join(_root, 'excel/0830/excel_pair_crop_val.csv'),
+                                                                   transform=transforms)
+                # for validation in train phase
+                test_set_0908 = DATASET_REGISTRY.get(dataset_name)(img_dir=os.path.join(_root, 'excel/0908/shoe_crop_images'),
+                                                                   anno_path=os.path.join(_root, 'excel/0908/excel_pair_crop_val.csv'),
+                                                                   transform=transforms)
+                test_set = torch.utils.data.ConcatDataset((test_set_0830, test_set_0908))
+                
+            else:
+                test_set = DATASET_REGISTRY.get(dataset_name)(img_dir=os.path.join(_root, 'excel/0908/shoe_crop_images'),
+                                                             anno_path=os.path.join(_root, 'excel/0908/excel_pair_crop_val.csv'),
+                                                             transform=transforms)
+                
         if comm.is_main_process():
             if dataset_name == 'ShoeDataset':
                 data.show_test()
