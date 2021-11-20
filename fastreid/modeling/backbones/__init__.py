581bddbbdc7fdf40b494665e2b102dff17c69ccf
l1aoxingyu <sherlockliao01@gmail.com>
2021-11-20 10:52:06 +0800

    register mobilenetv3

--- fastreid/modeling/backbones/__init__.py
+++ fastreid/modeling/backbones/__init__.py
@@ -13,5 +13,6 @@ from .resnext import build_resnext_backbone
 from .regnet import build_regnet_backbone, build_effnet_backbone
 from .shufflenet import build_shufflenetv2_backbone
 from .mobilenet import build_mobilenetv2_backbone
+from .mobilenetv3 import build_mobilenetv3_backbone
 from .repvgg import build_repvgg_backbone
 from .vision_transformer import build_vit_backbone
